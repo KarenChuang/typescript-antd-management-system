b5db654a6052f4eca0bb854868a4dc4f23e24976
KarenChuang <magic_gua@163.com>
2019-01-05 17:15:41 +0800

    :bug: fix nav menu bugs

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Switch, BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { StyledLayout, Logo, TriggerIcon } from './Styles'
 import Dashboard from '../views/Dashboard'
+import List from '../views/List'
 
 import {
   Layout, Menu, Icon,
@@ -13,91 +14,104 @@ const {
 
 const routes = [
   {
-    path: "/dashboard",
+    path: "/",
     exact: true,
     main: () => <Dashboard />
   },
   {
     path: "/list",
-    main: () => <h2>list</h2>
+    main: () => <List />
   },
   {
     path: "/submit",
     main: () => <h2>submit</h2>
   }
 ];
-class LayoutContainer extends React.Component {
-  
-    state = {
-      collapsed: false
-    };
 
-    toggle = () => {
-      this.setState({
-        collapsed: !this.state.collapsed,
-      });
-    }
-    
-    onCollapse = (collapsed: boolean) => {
-      this.setState({ collapsed });
-    }
+interface IStates {
+  collapsed: boolean;
+  selectedKeys: string[]
+}
+class LayoutContainer extends React.Component<{}, IStates> {
 
-    render() {
-      return (
-        <Router>
+  constructor(props: {}) {
+    super(props)
+    const selectedKey = window.location.pathname.split('/')[1];
+    this.state = {
+      collapsed: false,
+      selectedKeys: [selectedKey || 'dashboard']
+    }
+  }
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+
+  onCollapse = (collapsed: boolean) => {
+    this.setState({ collapsed });
+  }
+
+  render() {
+    return (
+      <Router>
+        <StyledLayout>
+          <Sider
+            trigger={null}
+            collapsible={true}
+            collapsed={this.state.collapsed}
+          >
+            <Logo />
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={this.state.selectedKeys}>
+              <Menu.Item key="dashboard">
+                <Link to="/">
+                  <Icon type="user" />
+                  <span>Dahboard</span>
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="list">
+                <Link to="/list">
+                  <Icon type="video-camera" />
+                  <span>List</span>
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="submit">
+                <Link to="/submit">
+                  <Icon type="upload" />
+                  <span>Submit</span>
+                </Link>
+              </Menu.Item>
+            </Menu>
+          </Sider>
           <StyledLayout>
-            <Sider
-              trigger={null}
-              collapsible={true}
-              collapsed={this.state.collapsed}
+            <Header style={{ background: '#fff', padding: 0 }}>
+              <TriggerIcon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                onClick={this.toggle}
+                />
+            </Header>
+            <Content style={{
+              margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280,
+            }}
             >
-              <Logo />
-              <Menu theme="dark" mode="inline" defaultSelectedKeys={['dashboard']}>
-                <Menu.Item key="dahboard">
-                  <Link to="/dashboard">
-                    <Icon type="user" />dashboard
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="list">
-                  <Link to="/list">
-                    <Icon type="video-camera" />list
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="submit">
-                  <Link to="/submit">
-                    <Icon type="upload" />submit
-                  </Link>
-                </Menu.Item>
-              </Menu>
-            </Sider>
-            <StyledLayout>
-              <Header style={{ background: '#fff', padding: 0 }}>
-                <TriggerIcon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-                  onClick={this.toggle}
-                  />
-              </Header>
-              <Content style={{
-                margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280,
-              }}
-              >
-              <Switch>
-                {routes.map((route, index) => (
-                  // Render more <Route>s with the same paths as
-                  // above, but different components this time.
-                  <Route
-                    key={index}
-                    path={route.path}
-                    exact={route.exact}
-                    component={route.main}
-                  />
-                ))}
-              </Switch>
-              </Content>
-            </StyledLayout>
+            <Switch>
+              {routes.map((route, index) => (
+                // Render more <Route>s with the same paths as
+                // above, but different components this time.
+                <Route
+                  key={index}
+                  path={route.path}
+                  exact={route.exact}
+                  component={route.main}
+                />
+              ))}
+            </Switch>
+            </Content>
           </StyledLayout>
-        </Router>
-      );
-    }
+        </StyledLayout>
+      </Router>
+    );
+  }
 }
   
 export default LayoutContainer;
---
+++ src/views/List.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { List, Card } from 'antd';
+
+const data: Array<{ title: string }> = [
+  {
+    title: 'Title 1',
+  },
+  {
+    title: 'Title 2',
+  },
+  {
+    title: 'Title 3',
+  },
+  {
+    title: 'Title 4',
+  },
+  {
+    title: 'Title 5',
+  },
+  {
+    title: 'Title 6',
+  },
+];
+
+class PageList extends React.Component {
+  renderListItem = (item: { title: string }) => (<List.Item>
+    <Card title={item.title}>Card content</Card>
+  </List.Item>
+  )
+
+  render() {
+    return (
+      <div>
+        <List
+            grid={{
+              gutter: 16, xs: 1, sm: 2, md: 4, lg: 4, xl: 6, xxl: 3,
+            }}
+            dataSource={data}
+            renderItem={this.renderListItem}
+          />
+      </div>
+    );
+  }
+}
+
+export default PageList;
